e1b9d8c703cb3d9fb50dcc306044e50079a8f2ff
Max Lovell <max_lovell@hotmail.co.uk>
2022-02-20 04:43:27 +0000

    Delete iframe_embed.js

--- iframe_embed.js
+++
@@ -1,7 +0,0 @@
-Qualtrics.SurveyEngine.addOnload(function(){
-	var qthis = this;
-    qthis.hideNextButton();
-    jQuery('body').append('<iframe src="https://users.sussex.ac.uk/mel29/experiments/metacog_dots/dots_browser_test.html" name="frame1" id="frame1" allowfullscreen></iframe>');
-});
-Qualtrics.SurveyEngine.addOnReady(function(){});
-Qualtrics.SurveyEngine.addOnUnload(function(){});
